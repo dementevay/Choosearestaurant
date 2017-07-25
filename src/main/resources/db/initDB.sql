4b1217356a34ee7abceeb189ea56dda8018e9993
Dementevay <dementevay@gmail.com>
2017-07-25 16:10:40 +0300

    init and populate db - sql

---
+++ src/main/resources/db/initDB.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS vote;
+DROP TABLE IF EXISTS restaurants;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START 100000;
+
+CREATE TABLE users
+(
+  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name       VARCHAR NOT NULL,
+  email      VARCHAR NOT NULL,
+  password   VARCHAR NOT NULL,
+  enabled    BOOL DEFAULT TRUE
+);
+/*CREATE UNIQUE INDEX users_unique_email_idx ON users (email);*/
+
+CREATE TABLE user_roles
+(
+  role    VARCHAR,
+  user_id INTEGER NOT NULL,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE restaurants (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name        VARCHAR NOT NULL
+);
+
+CREATE TABLE meals (
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  restaurant_id   INTEGER NOT NULL,
+  date_time       TIMESTAMP NOT NULL,
+  name            TEXT NOT NULL,
+  price           INT NOT NULL,
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants (id) ON DELETE CASCADE
+);
+/*CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals(restourant_id, date_time)*/
+
+CREATE TABLE vote (
+  id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  date_time       TIMESTAMP NOT NULL,
+  user_id         INTEGER NOT NULL,
+  restaurant_id   INTEGER NOT NULL
+  /*FOREIGN KEY (restaurant_id) REFERENCES restaurants (id),*/
+  /*FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE*/
+);
---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,47 @@
+DELETE FROM user_roles;
+DELETE FROM meals;
+DELETE FROM users;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+-- user1
+INSERT INTO users (name, email, password, enabled)
+VALUES ('User1', 'user1@yandex.ru', 'password1', TRUE );
+-- user2
+INSERT INTO users (name, email, password, enabled)
+VALUES ('User2', 'user2@yandex.ru', 'password2', TRUE );
+-- user3
+INSERT INTO users (name, email, password, enabled)
+VALUES ('User3', 'user3@yandex.ru', 'password3', TRUE );
+-- admin
+INSERT INTO users (name, email, password, enabled)
+VALUES ('Admin', 'admin@gmail.com', 'password', TRUE );
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_USER', 100001),
+  ('ROLE_USER', 100002),
+  ('ROLE_ADMIN', 100003);
+
+INSERT INTO restaurants (name) VALUES
+  ('Берёзка'),
+  ('McBurger'),
+  ('Prague');
+
+INSERT INTO meals (restaurant_id, date_time, name, price) VALUES
+  ('100004','2017-07-26 10:00:00', 'Борщ', 100),
+  ('100004','2017-07-26 10:00:00', 'Шашлык', 200),
+  ('100004','2017-07-26 10:00:00', 'Картофель жаренный', 50),
+  ('100004','2017-07-26 10:00:00', 'Водка', 150),
+  ('100005','2017-07-26 10:00:00', 'happy fat', 120),
+  ('100005','2017-07-26 10:00:00', 'hamburger', 80),
+  ('100005','2017-07-26 10:00:00', 'Greek salad', 200),
+  ('100005','2017-07-26 10:00:00', 'potatoes', 90),
+  ('100006','2017-07-26 10:00:00', 'Окрошка', 2500),
+  ('100006','2017-07-26 10:00:00', 'Котлета от шефа', 1500),
+  ('100006','2017-07-26 10:00:00', 'Морс', 800),
+  ('100006','2017-07-26 10:00:00', 'Хлеб', 250);
+
+INSERT INTO vote (date_time, user_id, restaurant_id) VALUES
+  ('2017-07-26 10:00:00', 100000, 100006),
+  ('2017-07-26 10:00:00', 100001, 100004),
+  ('2017-07-26 10:00:00', 100002, 100006);
